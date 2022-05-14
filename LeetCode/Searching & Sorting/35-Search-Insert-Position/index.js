ecadbd01ed854b903185a690579d2d3f6982fcb6
ryanweiler92 <ryanweiler92@gmail.com>
2022-05-14 09:52:38 -0400

    search insert position

---
+++ LeetCode/Searching & Sorting/35-Search-Insert-Position/index.js
@@ -0,0 +1,32 @@
+//BINARY SEARCH
+
+
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number}
+ */
+ var searchInsert = function(nums, target) {
+    
+    let firstIndex = 0
+    
+    let lastIndex = nums.length -1;
+    
+    while (firstIndex <= lastIndex){
+        
+        let midIndex = Math.floor((lastIndex + firstIndex) / 2);
+        
+        if(nums[midIndex] == target){
+
+        return midIndex;
+            
+        }else if (nums[midIndex] > target) {
+            lastIndex = midIndex - 1 
+        } else {
+            firstIndex  = midIndex + 1
+        }
+    }
+    
+    return firstIndex
+    
+};
